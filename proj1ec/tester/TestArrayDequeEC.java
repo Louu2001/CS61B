667b45d6cfc5f653607bc1d7aa3e2505191af339
Lou <1187602886@qq.com>
2024-07-10 16:54:37 +0800

    done with proj1ec

---
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -0,0 +1,43 @@
+package tester;
+
+import static org.junit.Assert.*;
+
+import afu.org.checkerframework.checker.signature.qual.SourceName;
+import edu.princeton.cs.introcs.StdRandom;
+import org.junit.Test;
+import student.StudentArrayDeque;
+
+public class TestArrayDequeEC {
+
+    @Test
+    public void testFailed() {
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+        String msg = "";
+        for (int i = 0; i < StdRandom.uniform(0, 1000000); i++) {
+            double choice = StdRandom.uniform();
+            Integer randVal = StdRandom.uniform(0, 100);
+            if (choice < 0.33) {
+                student.addLast(randVal);
+                solution.addLast(randVal);
+                msg += "addLast(" + randVal + ")\n";
+            } else if (choice < 0.67) {
+                student.addFirst(randVal);
+                solution.addFirst(randVal);
+                msg += "addFirst(" + randVal + ")\n";
+            } else {
+                int size = student.size();
+                msg += "size()\n";
+                if(size > 0){
+                    if(randVal <50){
+                        msg+="removeFirst()\n";
+                        assertEquals(msg,solution.removeFirst(),student.removeFirst());
+                    } else {
+                        msg +="removeLast()\n";
+                        assertEquals(msg,solution.removeLast(),student.removeLast());
+                    }
+                }
+            }
+        }
+    }
+}
